00d91ae62883f3ce08cf24f46c9eaf9243e41b9e
Daniel Suchy <suchydan@gmail.com>
2017-12-05 22:28:40 +0100

    Set up all tools

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import 'typeface-roboto' // eslint-disable-line
+import Button from 'material-ui/Button'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Button raised color="primary">
+          To get started
+        </Button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
